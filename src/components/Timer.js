25a0c3e117a17feeaa17cf5f366fecd785c99ea5
Brenden Thomas <brenden.thomas17@gmail.com>
2022-06-21 19:52:55 -0500

    -Implemented stop button -Changed layout to allow for increasing the individual minutes -Added (hopefully helpful) comments

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,47 +1,62 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 
+//Will be used to store the interval for the displayed timer
+let storedInterval;
 
 const Timer =()=>{
     const [countdown, setcountdown] = useState('00:00');
-    const [increment, setincrement] = useState(0)
+    const [incrementMinute, setincrementMinute] = useState(0)
 
+
+//Starts the displayed timer countdown
     const startTimer =()=>{
         let startTime = new Date();
-        startTime.setSeconds(startTime.getSeconds() + increment);
-        let passedTime = setInterval(()=>{
+        startTime.setMinutes(startTime.getMinutes() +incrementMinute);
+        
+        storedInterval = setInterval(()=>{
             let total = Date.parse(startTime) - Date.parse(new Date());
-            let seconds = Math.floor((total / 1000) % 60);
-            setincrement(seconds);
-            setcountdown('00:'+(seconds >9 ? seconds: '0'+seconds))
+            const seconds = Math.floor((total / 1000) % 60);
+            const minutes = Math.floor((total / 1000 / 60) % 60);
+            setcountdown((minutes >9 ? minutes: '0'+minutes)+':'+(seconds >9 ? seconds: '0'+seconds))
             if(total === 0){
-                clearInterval(passedTime)
+                clearInterval(storedInterval)
             };
         },1000)
+
     }
 
     const increaseTime = () => {
-        setincrement((x)=>x+10);
-    }
-
-    const decreaseTime = () =>{
-        if(increment <= 9){
-            alert('cannot due negative time, amigo');
-        }else{
-            setincrement((x)=>x-10);
+        if(incrementMinute < 60 ){
+            setincrementMinute((x)=>x+1);
         }
     }
 
+    const decreaseTime = () =>{
+        if(incrementMinute <= 1){
+            alert('cannot due negative time, amigo');
+        }else{
+            setincrementMinute((x)=>x-1);
+        }
+    }
+
+    const stopTimer = () =>{
+        setincrementMinute(0);
+        setcountdown('00:00');
+        clearInterval(storedInterval)
+    }
+//When and only when incrementMinute's state updates, reflect that change on the display
     useEffect(() => {
-        setcountdown('00:'+(increment >9 ? increment: '0'+increment))
-    });
+        setcountdown((incrementMinute >9 ? incrementMinute: '0'+incrementMinute)+':00')
+    },[incrementMinute]);
     
     return(
         <div>
             <h2 id="h2Holder">{countdown}</h2>
-            <button type="button" id="increaseTime" onClick={increaseTime}> + 10 seconds</button>
-            <button type="button" id="decreaseTime" onClick={decreaseTime}> - 10 seconds</button>
+            <button type="button" id="increaseTime" onClick={increaseTime}> + 1 minute</button>
+            <button type="button" id="decreaseTime" onClick={decreaseTime}> - 1 minute</button>
             <button type="button" id="startCountdown" onClick={startTimer}> start</button>
+            <button type="button" id="cancelCountdown" onClick={stopTimer}> stop</button>
         </div>
 
     );
